e51fe8d37b8fb6f2e75d9f186c8bbcb3ff565c73
Toni Helminen <kalleankka1@gmail.com>
2020-10-26 23:58:46 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,39 +1,34 @@
-# Overview
-sacrifice is a simple and lightweight JSON library written in JavaScript
-
 ![sacrifice](https://github.com/SamuraiDangyo/sacrifice/blob/master/logo.jpg)
 
-## Install
-All you need is sacrifice.js. No complex build systems. Just 5kB. Nothing else.
+# Overview
+sacrifice. Lightweight JSON library written in vanilla JavaScript
+
+## Installation
+All you need is sacrifice.js. No complex build systems. Just ~5kB. Nothing else.
 ```
 <script src="sacrifice.js"></script>
 ```
 
 ## Examples
-Validate JSON string :
+#### Validate JSON string:
 ```
 sacrifice.validate("[42]");     // true
 sacrifice.validate("[ 42 ] s"); // false
 ```
 
-Trim JSON string :
+#### Trim JSON string:
 ```
 sacrifice.trim("[42, [ 42  ]]"); // "[42,[42]]"
 ```
 
-JSON to string :
+#### JSON to string:
 ```
 sacrifice.stringify([42, {"key": 42}]);  // "[42, {"key": 42}]"
 sacrifice.stringify([42, function(){}]); // undefined ( see: sacrifice.errorMsg )
 ```
 
-String to JSON :
+#### String to JSON:
 ```
 sacrifice.parse("42");  // "42"
 sacrifice.parse("4 2"); // undefined ( see: sacrifice.errorMsg )
-```
-
-Show version :
-```
-sacrifice.version(); // "sacrifice 1.0"
 ```
--- unittests.js
+++ unittests.js
@@ -1,5 +1,5 @@
 /*
-sacrifice, a simple and lightweight JSON library written in JavaScript
+sacrifice. Lightweight JSON library written in vanilla JavaScript
 Copyright (C) 2020 Toni Helminen
 */
 
@@ -8,96 +8,38 @@ Copyright (C) 2020 Toni Helminen
 (function(){
 'use strict';
 
-function unittest1() {
-  [
-    true,
-    false,
-    null,
-    42,
-    42e4,
-    [null," ",42],
-    [3],
-    [3,"42",42,["42"]],
-    [3,null,42],
-    [[{"key": 42},null,42]],
-    [[333,3,null,[333,3,null]]],
-  ].forEach(function(test){if (sacrifice.stringify(test) != JSON.stringify(test)) console.error(sacrifice.stringify(test), JSON.stringify(test));});
+function unittestStringify() {
+  [true, false, null, 42, 42e4, [null," ",42], [3], [3,"42",42,["42"]], [3,null,42], [[{"key": 42},null,42]], [[333,3,null,[333,3,null]]]]
+  .forEach(function(test){if (sacrifice.stringify(test) != JSON.stringify(test)) console.error(sacrifice.stringify(test), "<>", JSON.stringify(test));});
 }
 
-function unittest2() {
-  [
-    ' true',
-    'null ',
-    ' false',
-    ' 42',
-    ' 42.42  ',
-    '[ null ]',
-    '[ true, [ true ] ]',
-    '[ false, false ]',
-    '[ 42 ]',
-    '[ null ]',
-    '[ true ]',
-    '[ false, false ]',
-    '[ 42 ]',
-    '[4,3,{"sss":3},[5],[3,7]]',
-    '{"key" : false}',
-    '{"key" : true}',
-    '{"key" : null}',
-    '{"key" : 42}',
-    '{"key" : 42.01}',
-    '{"key" : 4.2e+30}',
-    '{"key" : -4.21e+30}',
-    '{"key" : "value_a"}',
-    '["value_1", "value_2"]',
-    '{"key_a" : "value_a", "keyb" : "value_b", "arrays" : ["one", "2", "42"]}',
-    '{"key_a" : {"key_a" : "value_a"}, "keyb" : "value_b"}'
-  ].forEach(function(test){if (sacrifice.trim(test) != JSON.stringify(JSON.parse(test))) console.error(test, sacrifice.trim(test), JSON.stringify(JSON.parse(test)));});
+function unittestTrim() {
+  [' true', 'null ', ' false', ' 42', ' 42.42  ', '[ null ]', '[ true, [ true ] ]', '[ false, false ]', '[ 42 ]', '[ null ]', '[ true ]', '[ false, false ]', '[ 42 ]', '[4,3,{"sss":3},[5],[3,7]]', '{"key" : false}',
+    '{"key" : true}', '{"key" : null}', '{"key" : 42}', '{"key" : 42.01}', '{"key" : 4.2e+30}', '{"key" : -4.21e+30}', '{"key" : "value_a"}', '["value_1", "value_2"]',
+    '{"key_a" : "value_a", "keyb" : "value_b", "arrays" : ["one", "2", "42"]}', '{"key_a" : {"key_a" : "value_a"}, "keyb" : "value_b"}']
+  .forEach(function(test){if (sacrifice.trim(test) != JSON.stringify(JSON.parse(test))) console.error(test, sacrifice.trim(test), "<>", JSON.stringify(JSON.parse(test)));});
 }
 
-function unittest3() {
-  [
-    true,
-    false,
-    [false],
-    [[false]],
-    null,
-    'null',
-    '[null,[null],[null]]',
-    '[true,[true],[true]]',
-    '[42,[42],[42]]',
-    '{"key": 42}'
-  ].forEach(function(test){if (sacrifice.stringify(sacrifice.parse(test)) != JSON.stringify(JSON.parse(test))) console.error(test, sacrifice.stringify(sacrifice.parse(test)), JSON.stringify(JSON.parse(test)));});
+function unittestParseStringify() {
+  [true, false, [false], [[false]], null, 'null', '[null,[null],[null]]', '[true,[true],[true]]', '[42,[42],[42]]', '{"key": 42}']
+  .forEach(function(test){if (sacrifice.stringify(sacrifice.parse(test)) != JSON.stringify(JSON.parse(test))) console.error(test, sacrifice.stringify(sacrifice.parse(test)), "<>", JSON.stringify(JSON.parse(test)));});
 }
 
-function unittest4() { // Bad JSON
-  [
-    '[444,   7    77]',
-    '{"sds": 42 "sds": 42}',
-    '[ 3 ] s',
-    '[4,3,3,,]',
-    '[4,3,3,]',
-    '[4,3,{"sss":3,},[5],[3,7]]',
-    'nulli',
-    '["key_a" : false}',
-    '[false : false]',
-    '{false : false}',
-    '4 7'
-  ].forEach(function(test){if (sacrifice.validate(test)) console.error(test, sacrifice.validate(test));});
+function unittestBadJson() {
+  ['[444,   7    77]', '{"sds": 42 "sds": 42}', '[ 3 ] s', '[4,3,3,,]', '[4,3,3,]', '[4,3,{"sss":3,},[5],[3,7]]', 'nulli', '["key_a" : false}', '[false : false]', '{false : false}', '4 7']
+  .forEach(function(test){if (sacrifice.validate(test)) console.error(test, sacrifice.validate(test));});
 }
 
-function unittest5() { // Good JSON
-  [
-    '" "',
-    '[ 3 ]'
-  ].forEach(function(test){if ( ! sacrifice.validate(test)) console.error(test, sacrifice.validate(test));});
+function unittestGoodJson() {
+  ['" "', '[ 3 ]'].forEach(function(test){if (!sacrifice.validate(test)) console.error(test, sacrifice.validate(test));});
 }
 
 sacrifice.unittests = function() {
-  unittest1();
-  unittest2();
-  unittest3();
-  unittest4();
-  unittest5();
+  unittestStringify();
+  unittestTrim();
+  unittestParseStringify();
+  unittestBadJson();
+  unittestGoodJson();
 };
 
 })();
